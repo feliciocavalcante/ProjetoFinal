1a1a710ce8c86c03daca4fdc05b4f20ab0df3aa4
feliciocavalcante <feliciospfc@gmail.com>
2025-05-10 10:29:40 -0300

    atualização de responsividade

--- pageHome/HomeScript/script.js
+++ pageHome/HomeScript/script.js
@@ -94,3 +94,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 5000)
   })
 })
+
+const toggleButton = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+
+toggleButton.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+
